ba0a256470bd2936a5668f9ebc43e865445318d2
alpipego <alpipego@gmail.com>
2015-09-16 18:57:33 +0200

    simplified header, added plugin url, author

--- landing-page.php
+++ landing-page.php
@@ -2,18 +2,17 @@
 
 namespace QuanDigital\LandingPage;
 
+use QuanDigital\WpLib\Autoload;
+
 /**
  * Plugin Name: Quan Landing Page
- * Plugin URI: http://www.quandigital.com/
+ * Plugin URI: https://github.com/quandigital/wp-quan-landing-page
+ * Author: Quan Digital GmbH
+ * Author URI: http://www.quandigital.com/
  * Description: Adds a custom post types for landing pages
  * Version: 1.0.0
  * License: MIT
  */
-
-defined('ABSPATH') or die();
-// include ABSPATH . '../../vendor/autoload.php';
-
-use QuanDigital\WpLib\Autoload;
 
 new Autoload(__DIR__, __NAMESPACE__);
 
